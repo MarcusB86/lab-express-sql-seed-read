37b6448590f04489fdb8512b443ab20178d6c694
MarcusB86 <marcusb08@gmail.com>
2024-08-05 17:53:50 -0400

    Create and Show complete with validations

--- back-end/controllers/songController.js
+++ back-end/controllers/songController.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const songs = express.Router()
-const { getAllSongs } = require("../queries/songs");
+const { getAllSongs, getSong, createSong } = require("../queries/songs");
+const { checkName, checkBoolean } = require("../validations/checkSongs.js");
 
 songs.get("/", async (req, res) => {
     const allSongs = await getAllSongs();
@@ -10,5 +11,18 @@ songs.get("/", async (req, res) => {
         res.status(500).json({ error: "server error" })
     }
   });
+  songs.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const song = await getSong(id);
+    if(song) {
+        res.json(song);
+    } else {
+        res.status(404).json({ error: "not found" })
+    }
+  });
+  songs.post("/", checkBoolean, checkName, async (req, res) => {
+    const song = await createSong(req.body);
+    res.json(song);
+  })
 
 module.exports = songs;
